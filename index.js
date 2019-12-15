bfdfd2c6a73570cd5a5efab2d6ae848c0cbdf45d
frantchessico <jaimeinoque20@gmail.com>
2019-12-15 17:06:46 +0200

    primerio commit de swipper.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94
+    },  
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    } 
+});
